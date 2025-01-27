aea18d0ca03873779b100be5c8a68755d1db767c
PolyVectors <PolyVector@protonmail.com>
2025-01-27 02:36:13 +0000

    Get rid of procedure testing

--- src/main.asm
+++ src/main.asm
@@ -7,11 +7,7 @@ include 'macros/lang.inc'
 hello: db "Hello, world!", 10
 hello_len = $-hello
 
-print:
-    write STDOUT_FILENO, hello, hello_len
-    ret
-
 entry _start
 _start:
-    call print
+    write STDOUT_FILENO, hello, hello_len
     exit 0
